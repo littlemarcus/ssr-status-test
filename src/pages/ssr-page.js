76f04b9087098ddf1c75730779ac19fbfe0a9be2
Marcus Little <mcolelittle@gmail.com>
2024-04-08 13:57:01 -0400

    initial commit

---
+++ src/pages/ssr-page.js
@@ -0,0 +1,49 @@
+import * as React from "react"
+
+const UsingSSR = ({ serverData }) => {
+  return (
+    <>
+      <h1>
+        This page is <b>rendered server-side</b>
+      </h1>
+      <p>
+        This page is rendered server side every time the page is requested.
+        Reload it to see a(nother) random photo from{" "}
+        <code>dog.ceo/api/breed/shiba/images/random</code>:
+      </p>
+      <img
+        style={{ width: "320px", borderRadius: "var(--border-radius)" }}
+        alt="A random dog"
+        src={serverData.message}
+      />
+      <p>
+        To learn more, head over to our{" "}
+        <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/server-side-rendering/">
+          documentation about Server Side Rendering
+        </a>
+        .
+      </p>
+    </>  
+  )
+}
+
+export default UsingSSR
+
+export async function getServerData() {
+  try {
+    const res = await fetch(`https://dog.ceo/api/breed/shiba/images/random`)
+    if (!res.ok) {
+      throw new Error(`Response failed`)
+    }
+    return {
+      props: await res.json(),
+      status: 404
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      headers: {},
+      props: {},
+    }
+  }
+}
